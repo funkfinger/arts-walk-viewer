2128ea6f485cf7df175f182177bebcf1b1783f84
dilandesai <dilandesai9@gmail.com>
2023-04-27 09:04:51 -0700

    a

---
+++ data/data.js
@@ -0,0 +1,12 @@
+export const ProjectData = [
+    {
+        title: "Dilan Desai Project",
+        id: 1,
+        link: "https://big-d-artswalk.netlify.app/"
+    },
+    {
+        title: "Dilan Desai Project 2",
+        id: 2,
+        link: "https://big-d-artswalk.netlify.app/"
+    }
+]
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import { Inconsolata } from "next/font/google";
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
+import { ProjectData } from '../data/data'
+import Link from "next/link";
 
 const font = Inconsolata({ subsets: ["latin"] });
 
@@ -11,6 +15,21 @@ const Home = () => (
       </h1>
     </heading>
     <main>Welcome</main>
+    <Carousel>
+      {
+        ProjectData.map((project) => (
+          <div className="border-solid border-2 w-2/3 h-300 text-center rounded-md" key={project.id}>
+            <h3 className="text-lg">{project.title}</h3>
+            <br/>
+              <a href={project.link} target="_blank">
+                Open Project
+              </a>
+            <br />
+            <iframe src={project.link} />
+          </div>
+        ))
+      }
+    </Carousel>
   </div>
 );
 export default Home;
